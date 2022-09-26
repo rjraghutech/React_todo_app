1c75c1489a5b3dd0a5a53435223df10655d6ae8c
RJ_Raghu_tech <91701745+rjraghutech@users.noreply.github.com>
2022-09-26 21:57:36 +0530

    Commit 6

--- todo_app2/src/App.js
+++ todo_app2/src/App.js
@@ -2,15 +2,9 @@ import './App.css';
 import Todo from "./Components/Todo";
 
 function App() {
-  let todo =[
-    {
-      title: "Eat",
-      desc: "Have breakfast, lunch and dinner.."
-    }
-  ]
   return (
     <>
-      <Todo onDelete={onDelete}/>
+      <Todo/>
     </>
   );
 };
--- todo_app2/src/Components/Todo.js
+++ todo_app2/src/Components/Todo.js
@@ -1,14 +1,47 @@
-import React from "react";
-import TodoItem from "./TodoItem"
+import React, { useState } from "react";
 
-export default function Todo(props) {
+export default function Todo() {
+    const [task, setTask] = useState([]);
+    const [items, setItems] = useState("");
+    //  const [todoedit,setEdit]=useState(null)
+    //  const [todosave,setSave]=useState("")
+    const handleadd = (e) => {
+
+        setTask([...task, items]);
+        setItems("")
+    }
+
+
     return (
-        <div className="main-cont">
-            <h1>List your TODO's here.....😍😍</h1>
-            {props.todo.map((todo)=> {
-                return <TodoItem todo={todo} onDelete={props.onDelete}/>
-            })}
-            
-        </div>
+        <>
+            <h1>My TODO Application.....😍😍😍</h1>
+            <input
+                type="text" className="input1"
+                onChange={(e) => setItems(e.target.value)}
+                defaultValue={task}
+            />
+            <button className="btn" onClick={handleadd}>Add task</button>
+            <div className="container">
+                <h2>My TODO Items...</h2>
+                {task.map((tasks) => (
+                    <div className="innercontainer" key={tasks}>
+                        <input type="text" className="input" defaultValue={tasks} readOnly/>
+                        <button className="btn btn2"
+                            onClick={() => {
+                                let deleteitems = task.filter((x) => x !== tasks);
+                                setTask([...deleteitems])
+                                // console.log(deleteitem);
+                            }}>
+                            Del Task
+                        </button>
+                        {<button className="btn btn1" 
+                        onClick={()=>{
+                            let innertext = document.querySelector('.input');
+                            innertext.removeAttribute("readOnly");
+                        }}>Edit Task</button>}
+                    </div>
+
+                ))}</div>
+        </>
     );
 }
